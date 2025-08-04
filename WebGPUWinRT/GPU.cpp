e53a7e4bfaf53219941aeed9a175bebb64f883a7
Xiang Hong <hx.hongxiang@gmail.com>
2025-08-04 19:20:26 +0800

    work in progress

--- WebGPUWinRT/GPU.cpp
+++ WebGPUWinRT/GPU.cpp
@@ -6,7 +6,7 @@
 #include "GPUAdapter.h"
 #include <winrt/Windows.Foundation.h>
 #include <future>
-
+#include <wil\resource.h>
 
 namespace winrt::WebGPUWinRT::implementation
 {
@@ -28,30 +28,58 @@ namespace winrt::WebGPUWinRT::implementation
 		//};
 		//auto userData{ std::make_shared<UserData>() };
 
-		auto promise = std::make_shared<std::promise<WGPUAdapter>>();
+		struct CallbackData {
+			WGPUAdapter adapter{ nullptr };
+			std::string errorMessage;
+
+			// Create a manual-reset event using WIL.
+			// This is the direct replacement for:
+			// CreateEvent(nullptr, TRUE, FALSE, nullptr)
+			wil::shared_event ready{ wil::EventOptions::ManualReset };
+		};
+		auto data = std::make_shared<CallbackData>();
+
+
+		//if (data.status != 0)
+		//{
+		//	winrt::throw_hresult(E_FAIL, winrt::to_hstring(data.errorMessage));
+		//}
+
+		//co_return data.resultObject;
+
+		//auto promise = std::make_shared<std::promise<WGPUAdapter>>();
 
 		auto callback = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, WGPUStringView message, void* userdata, void* _) {
+			auto* data = static_cast<CallbackData*>(userdata);
+
 			auto p = static_cast<std::promise<WGPUAdapter>*>(userdata);
 			if (status == WGPURequestAdapterStatus_Success)
 			{
-				std::cout << "Got adapter in async using resume background" << std::endl;
-				p->set_value(adapter);
+				std::cout << "Got adapter in async using resume background using event" << std::endl;
+				data->adapter = adapter;
 			}
 			else
 			{
-				p->set_exception(std::make_exception_ptr(std::runtime_error(interop::to(message))));
+				data->errorMessage = interop::to(message);
 			}
+			data->ready.SetEvent();
 			};
 
 		wgpuInstanceRequestAdapter(handle, &adapterOpts, {
 			.callback = callback,
-			.userdata1 = promise.get(),
+			.userdata1 = data.get(),
 			});
 
-		// TODO: use correct async implementation
-		co_await winrt::resume_background();
-		auto result = promise->get_future().get();
-		co_return make<implementation::GPUAdapter>(result);
+		co_await winrt::resume_on_signal(data->ready.get());
+
+		if (data->adapter == nullptr)
+		{
+			auto error_message = winrt::to_hstring(data->errorMessage);
+			throw winrt::hresult_error(E_FAIL, error_message);
+		}
+		else {
+			co_return make<implementation::GPUAdapter>(data->adapter);
+		}
 	}
 
 	winrt::Windows::Foundation::IAsyncOperation<winrt::WebGPUWinRT::IGPUAdapter> GPU::RequestAdapter(winrt::WebGPUWinRT::GPURequestAdapterOptions options)
